4f8042d2b77ced3e24cda6f18cc7fcf19ee2ea7b
Daniel Perez <bryan.bp4@gmail.com>
2022-04-20 01:11:17 -0500

    update react

--- src/Login.js
+++ src/Login.js
@@ -1,31 +1,34 @@
 import React, {Component}from 'react'
+import logo from './img/hat.png'
 import './Login.css';
 
 class Login extends Component{
     render(){
         return(
-        <div className='container glass'>
-            <form className='form'>
-                <img src='./img/menu.png' alt='logo' className='img'></img>
-                <div className='form-group'>
-                    <input autoComplete='off' id='user' className='input glass' type='text' placeholder='a'></input>
-                    <label htmlFor='user' className='label'>Usuario</label>
-                </div>
-                <div className='form-group'>
-                    <input autoComplete='off' id='password' className='input glass' type='password' placeholder='a'></input>
-                    <label htmlFor='password' className='label'>Contraseña</label>
-                </div>
-                <input className='button glass' type='submit' value='Iniciar sesion'></input>
-                <div className='links'>
-                <a href="#">¿Usuario nuevo? Registrate</a>
-                <a href="#">¿Olvidaste tu contraseña?</a>
-                </div>
-            </form>
-            <div className='box box1 glass'></div>
-            <div className='box box2 glass'></div>
-            <div className='box box3 glass'></div>
-            <div className='box box4 glass'></div>
-            <div className='box box5 glass'></div>
+        <div className='contpos'>
+            <div className='container glass'>
+                <form className='form'>
+                    <img src={logo} alt='logo' className='img'/>
+                    <div className='form-group'>
+                        <input autoComplete='off' id='user' className='input glass' type='text' placeholder='a'></input>
+                        <label htmlFor='user' className='label'>Usuario</label>
+                    </div>
+                    <div className='form-group'>
+                        <input autoComplete='off' id='password' className='input glass' type='password' placeholder='a'></input>
+                        <label htmlFor='password' className='label'>Contraseña</label>
+                    </div>
+                    <input className='button glass' type='submit' value='Iniciar sesion'></input>
+                    <div className='links'>
+                    <a href="#">¿Usuario nuevo? Registrate</a>
+                    <a href="#">¿Olvidaste tu contraseña?</a>
+                    </div>
+                </form>
+            </div>
+                <div className='box box1 glass'></div>
+                <div className='box box2 glass'></div>
+                <div className='box box3 glass'></div>
+                <div className='box box4 glass'></div>
+                <div className='box box5 glass'></div>
         </div>
         )
     }
